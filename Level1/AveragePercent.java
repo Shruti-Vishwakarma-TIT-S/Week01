9497714535c165460cbf8077d070f028194a0f74
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-01-09 19:38:16 +0530

    average

---
+++ Level1/AveragePercent.java
@@ -0,0 +1,19 @@
+//create class name AveragePercent
+public class AveragePercent{
+	public static void main(String[] args){
+	//take Sam's marks in mathematics
+	int marksMaths = 94;
+	
+	//take marks in Physics
+	int marksPhysics = 95;
+	
+	//take marks in Chemistry
+	int marksChemistry = 96;
+	
+	//Sam's Average Percentage Mark
+	 double averagePercentage = (marksMaths + marksPhysics + marksChemistry)/3.0;
+	
+	//print the output be like
+	System.out.println("Sam's average mark in PCM is " + averagePercentage);
+	}
+}
