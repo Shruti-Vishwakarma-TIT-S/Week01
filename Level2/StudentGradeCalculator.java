c8291aa7613363e92bc94db21111bb4c9c072c08
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-01-11 15:09:04 +0530

    Student Grade Calculator

---
+++ Level2/StudentGradeCalculator.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+//create class StudentGradeCalculator
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Enter marks for three subjects
+        System.out.println("Enter marks for Physics, Chemistry, Mathematics ");
+        int physics = scanner.nextInt();
+        int chemistry = scanner.nextInt();
+        int mathematics = scanner.nextInt();
+
+        // Calculate average and percentage
+        int totalMarks = physics + chemistry + mathematics;
+        double averageMarks = totalMarks / 3.0;
+        double percentage = (totalMarks / 300.0) * 100;
+
+        // Determine grade and remarks
+        String grade, remarks;
+
+        if (percentage >= 80) {
+            grade = "A";
+            remarks = "Level 4, above agency-normalized standards";
+        } else if (percentage >= 70) {
+            grade = "B";
+            remarks = "Level 3, at agency-normalized standards";
+        } else if (percentage >= 60) {
+            grade = "C";
+            remarks = "Level 2, below, but approaching agency-normalized standards";
+        } else if (percentage >= 50) {
+            grade = "D";
+            remarks = "Level 1, well below agency-normalized standards";
+        } else if (percentage >= 40) {
+            grade = "E";
+            remarks = "Level 1-, too below agency-normalized standards";
+        } else {
+            grade = "R";
+            remarks = "Remedial standards";
+        }
+
+        // Display the results
+       
+        System.out.println("Average Marks: "+ averageMarks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Remarks: " + remarks);
+
+        scanner.close();
+    }
+}
